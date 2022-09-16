b458d6de9b2e25196b19073c0324b9970f0a8d51
RahulNeupane <nrahul503@gmail.com>
2022-09-16 11:35:55 +0545

    todo application

---
+++ save-todo.php
@@ -0,0 +1,13 @@
+<?php
+    include('./db_config.php');
+
+    if($_SERVER['REQUEST_METHOD']==='POST'){
+
+        // echo "<pre>";
+        // print_r($_POST);
+        // echo "</pre>";
+
+        $todo_item = $_POST['todo'];
+        echo "you have entered ". $todo_item;
+    }
+?>
